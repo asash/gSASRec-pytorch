b27014a7543e290c1ef5e7c731424bc1e0a03930
Aleksandr Petrov <firexel@gmail.com>
2023-09-05 21:44:18 +0100

    decreased dimensionality and num blocks

--- config_ml1m.py
+++ config_ml1m.py
@@ -3,11 +3,11 @@ from config import GSASRecExperimentConfig
 config = GSASRecExperimentConfig(
     dataset_name='ml1m',
     sequence_length=200,
-    embedding_dim=256,
+    embedding_dim=50,
     num_heads=1,
     max_batches_per_epoch=100,
-    num_blocks=3,
-    dropout_rate=0.1,
+    num_blocks=2,
+    dropout_rate=0.2,
     negs_per_pos=1,
     gbce_t = 0.0,
 )
