5a3f4c1b1124a9bd9d3a4b3597d71e1abafaa344
Sasha Petrov <firexel@gmail.com>
2023-09-06 12:22:31 +0000

    128 negs per positive

--- config_ml1m.py
+++ config_ml1m.py
@@ -3,7 +3,7 @@ from config import GSASRecExperimentConfig
 config = GSASRecExperimentConfig(
     dataset_name='ml1m',
     sequence_length=200,
-    embedding_dim=256, #consider decreasing dimensionality if you have OOMs
+    embedding_dim=128, #consider decreasing dimensionality if you have OOMs
     num_heads=1,
     max_batches_per_epoch=100,
     num_blocks=2,
