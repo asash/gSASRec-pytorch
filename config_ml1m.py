65a541971c0a47d20570a889358b0edbdaa2e133
Sasha Petrov <firexel@gmail.com>
2023-09-06 13:21:30 +0000

    increased dropout

--- config_ml1m.py
+++ config_ml1m.py
@@ -3,11 +3,11 @@ from config import GSASRecExperimentConfig
 config = GSASRecExperimentConfig(
     dataset_name='ml1m',
     sequence_length=200,
-    embedding_dim=128, #consider decreasing dimensionality if you have OOMs
+    embedding_dim=128,
     num_heads=1,
     max_batches_per_epoch=100,
     num_blocks=2,
-    dropout_rate=0.0,
+    dropout_rate=0.5,
     negs_per_pos=256,
     gbce_t = 0.75,
 )
